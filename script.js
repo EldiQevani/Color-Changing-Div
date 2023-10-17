b7986c623d73c4854937ff43c01dd32633977211
Eldi Qevani <you@example.com>
2023-10-17 11:32:21 +0200

    we will try to add the redo and undo functions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const square = document.querySelector(".square");
 const count = document.querySelector(".square  h1");
-const button = document.querySelector("button");
+const change = document.querySelector(".change");
 
-button.addEventListener("click", () => {
+change.addEventListener("click", () => {
   let nr = parseInt(count.innerText);
   nr++;
   count.innerText = nr;
 
   const changeColor = `hsl(${Math.random() * 360}, 90%, 70%)`;
-  const changeBoxShadow = `hsl(${Math.random() * 360}, 70%, 20%)`;
+  // const changeBoxShadow = `hsl(${Math.random() * 360}, 50%, 30%)`;
 
   square.style.backgroundColor = changeColor;
-  square.style.boxShadow = `0px 0px 20px 10px ${changeBoxShadow}`;
+  square.style.boxShadow = `0px 0px 20px 10px ${changeColor}`;
 });
